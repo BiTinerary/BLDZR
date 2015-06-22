1d611085584c4ea504032b29ad3f226b07cbd2be
Zack <izaac.corbett@gmail.com>
2015-06-22 17:07:38 -0500

    BIOS password hashes +

---
+++ BLDZR/BIOS Password Hash/ReadMe_files/recent_comments_widget.js
@@ -0,0 +1,79 @@
+
+
+document.write(' \
+<style type="text/css" media="screen">\
+	 .dsq-widget ul.dsq-widget-list {\
+	 padding: 0;\
+	 margin: 0;\
+	 text-align: left;\
+	 }\
+	 img.dsq-widget-avatar {\
+	 width: 32px;\
+	 height: 32px;\
+	 border: 0px;\
+	 margin: 0px;\
+	 padding: 0px 3px 3px 0px;\
+	 float: left;\
+	 }\
+	 a.dsq-widget-user {\
+	 font-weight: bold;\
+	 }\
+	 a.dsq-widget-thread {\
+	 font-weight: bold;\
+	 }\
+	 p.dsq-widget-meta {\
+	 clear: both;\
+	 font-size: 80%;\
+	 padding: 0;\
+	 margin: 0;\
+	 }\
+	 li.dsq-widget-item {\
+	 margin: 15px 0;\
+	 list-style-type: none;\
+	 clear: both;\
+	 }\
+	 span.dsq-widget-clout {\
+	 padding: 0 2px;\
+	 background-color: #ff7300;\
+	 color: #fff;\
+	 }\
+	 table.dsq-widget-horiz td {\
+	 padding-right: 15px;\
+	 }\
+	 .dsq-widget-comment p {\
+	 display: inline;\
+	 }\
+	 </style>\
+	 <ul class="dsq-widget-list">\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/disqus_iRJtVcpmuE/"><img class="dsq-widget-avatar" src="http://mediacdn.disqus.com/uploads/users/6791/3535/avatar92.jpg?1378866301"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/disqus_iRJtVcpmuE/">00111111</a>\
+	 <span class="dsq-widget-comment"><p>OMG THANKS BROOOOOOOOOOOOOOOOOO<br>its worked on a aspire 7738g. I was very very hopeless, that\'s maybe after 52 hours that i found this site. THANKS THANKS THANKS VERY MUTCH</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://dogber1.blogspot.com/2009/05/table-of-reverse-engineered-bios.html">Dogbert&#39;s Blog: BIOS Password Backdoors in Laptops</a>&nbsp;&middot;&nbsp;<a href="http://dogber1.blogspot.com/2009/05/table-of-reverse-engineered-bios.html#comment-1037944172">2 hours ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/marcoskenji/"><img class="dsq-widget-avatar" src="http://mediacdn.disqus.com/1378851429/images/noavatar92.png"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/marcoskenji/">Marcos kenji</a>\
+	 <span class="dsq-widget-comment"><p>need help with my laptop :/  Vaio pcg asking for OTP  password</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://dogber1.blogspot.com/2011/03/shmuck-of-month-sony.html">http://dogber1.blogspot.com/2011/03/shmuck-of-month-sony.html</a>&nbsp;&middot;&nbsp;<a href="http://dogber1.blogspot.com/2011/03/shmuck-of-month-sony.html#comment-1037811549">4 hours ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/disqus_azwlRLbird/"><img class="dsq-widget-avatar" src="http://mediacdn.disqus.com/uploads/users/7240/5394/avatar92.jpg?1378801328"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/disqus_azwlRLbird/">Terry</a>\
+	 <span class="dsq-widget-comment"><p>hello pls i have a dell studio 1558 laptop i have been using it, it works well all of a sudden it come on and goes off on it own, when it became steady it start showing anti-theft lock, it ask me...</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://dogber1.blogspot.com/2009/05/table-of-reverse-engineered-bios.html">Dogbert&#39;s Blog: BIOS Password Backdoors in Laptops</a>&nbsp;&middot;&nbsp;<a href="http://dogber1.blogspot.com/2009/05/table-of-reverse-engineered-bios.html#comment-1036821014">20 hours ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/dogber1/"><img class="dsq-widget-avatar" src="http://mediacdn.disqus.com/uploads/users/2125/4944/avatar92.jpg?1326753197"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/dogber1/">Dogbert</a>\
+	 <span class="dsq-widget-comment"><p>please use the lenovo support</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://dogber1.blogspot.com/2009/05/table-of-reverse-engineered-bios.html">Dogbert&#39;s Blog: BIOS Password Backdoors in Laptops</a>&nbsp;&middot;&nbsp;<a href="http://dogber1.blogspot.com/2009/05/table-of-reverse-engineered-bios.html#comment-1035717962">1 day ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/dogber1/"><img class="dsq-widget-avatar" src="http://mediacdn.disqus.com/uploads/users/2125/4944/avatar92.jpg?1326753197"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/dogber1/">Dogbert</a>\
+	 <span class="dsq-widget-comment"><p>please use the ibm/lenovo support</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://dogber1.blogspot.com/2009/05/table-of-reverse-engineered-bios.html">Dogbert&#39;s Blog: BIOS Password Backdoors in Laptops</a>&nbsp;&middot;&nbsp;<a href="http://dogber1.blogspot.com/2009/05/table-of-reverse-engineered-bios.html#comment-1035717795">1 day ago</a></p>\
+	 </li>\
+	 </ul>\
+');
